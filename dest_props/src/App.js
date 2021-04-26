a8bb14c4ca9f2657aee11e60935d5d7805e74239
Zainab6521 <zk.652162@gmail.com>
2021-04-26 12:50:18 +0500

    1st day!

---
+++ dest_props/src/App.js
@@ -0,0 +1,51 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React, {Component} from 'react'
+
+import './App.css'
+
+import Greet from './Components/Greet'
+import Welcome from './Components/Welcome'
+// import Hello from './Components/Hello'
+// import Message from './Components/Message'
+// import Counter from './Components/Counter'
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <Greet name= "diana" heroName="batman"/>
+        <div><h2>Boss! Bohat Mushkil horahi hai yahan., in starting only</h2>
+        </div>
+<Welcome/>
+      </div>
+    )
+  }
+}
+export default App;
+
