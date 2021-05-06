e339ad707946317bf1f4f18844485c5d6a15510c
Zainab6521 <zk.652162@gmail.com>
2021-05-06 12:28:56 +0500

    react routing started today

--- myroutedapp/src/App.js
+++ myroutedapp/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import Shop from './Components/Shop';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <div>
+        <Link to="/">Home </Link>
+        <Link to="/About">About Us </Link>
+        <Link to="/Shop">Shop Now </Link>
+      </div>
+      <Switch>
+        <Route path="/" exact>
+        <Home />
+          </Route>
+      <Route path="/About" component={About} />
+      <Route path="/Shop" Component={Shop} />
+      {/* <Route path="/" exact >
+        <Shop />
+        </Route> */}
+      <Route Component={Error} />
+      {/* <Home />
+        <Shop />
+        <About /> */}
+      </Switch>
+    </main >
+  )
 }
+// function Navbar() {
+//   return (
+//     <div>
+//       <Link to="/">Home </Link>
+//       <Link to="/About">About Us </Link>
+//       <Link to="/Shop">Shop Now </Link>
+//     </div>
+//   );
+// };
+// export { Navbar };
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header> */}
+
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ myroutedapp/src/Components/Shop.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Shop() {
+    return (
+      <div>
+        <h2>Shop now!</h2>
+      </div>
+    );
+  }
+
+export default Shop;
