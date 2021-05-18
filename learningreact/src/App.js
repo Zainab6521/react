d1192a8412ccaca4bb307d13acfd8c7f4763d637
Zainab6521 <zk.652162@gmail.com>
2021-05-18 16:28:08 +0500

    pro 1 by udemy

--- learningreact/src/App.js
+++ learningreact/src/App.js
@@ -1,11 +1,37 @@
-import './App.css';
-import ExpenseItems from './Components/ExpenseItems';
+import "./App.css";
+import Expenses from "./Components/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Learn React.",
+      amount: 261.67,
+      date: new Date(2021, 10, 8),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 261.67,
+      date: new Date(2021, 5, 18),
+    },
+    {
+      id: "e3",
+      title: "pick Abbiiiii",
+      amount: 100,
+      date: new Date(2021, 9, 19),
+    },
+    {
+      id: "e4",
+      title: "New MObile",
+      amount: 450000,
+      date: new Date(2022, 4, 22),
+    },
+  ];
   return (
     <div>
-      <h1>hello g! react parhlo.</h1>
-    <ExpenseItems></ExpenseItems>
+      <h1 className='head'>Learning react, ALhamDuLiLah</h1>
+      <Expenses items={expenses} />
     </div>
   );
 }
