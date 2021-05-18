2b1ec23a6a93b82952a613a3f3e7dc68ff679e42
Zainab6521 <zk.652162@gmail.com>
2021-05-18 11:47:06 +0500

    ....

--- myroutedapp/src/App.js
+++ myroutedapp/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
 import Home from './Components/Home';
 import About from './Components/About';
 import Shop from './Components/Shop';
@@ -18,7 +18,7 @@ function App() {
         <Home />
           </Route>
       <Route path="/About" component={About} />
-      <Route path="/Shop" Component={Shop} />
+      <Route path="/Shop" component={Shop} />
       {/* <Route path="/" exact >
         <Shop />
         </Route> */}
